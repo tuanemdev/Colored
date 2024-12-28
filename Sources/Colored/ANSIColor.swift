f906b929dd7ac47df6d395f7259faf433e4b1248
Nguyen Tuan Anh <nguyentuananh.developer@gmail.com>
2024-12-28 13:48:07 +0700

    Update 2

--- Sources/Colored/ANSIColor.swift
+++ Sources/Colored/ANSIColor.swift
@@ -9,9 +9,9 @@ public enum ANSIColor {
 public enum ColorType {
     /// Standard 16 Colors
     case standard(color: StandardColor, intensity: StandardIntensity = .standard)
-    /// Extended 256 Colors
+    /// Extended 256 Colors (8-bit color)
     case extended(color: UInt8)
-    /// RGB Colors
+    /// RGB Colors (24-bit color)
     case rgb(red: UInt8, green: UInt8, blue: UInt8)
 }
 
@@ -66,4 +66,25 @@ extension ANSIColor: ANSICode {
             }
         }
     }
+}
+
+// MARK: - Init ColorType
+public extension ColorType {
+    static var black:          Self { .standard(color: .black, intensity: .standard) }
+    static var red:            Self { .standard(color: .red, intensity: .standard) }
+    static var green:          Self { .standard(color: .green, intensity: .standard) }
+    static var yellow:         Self { .standard(color: .yellow, intensity: .standard) }
+    static var blue:           Self { .standard(color: .blue, intensity: .standard) }
+    static var magenta:        Self { .standard(color: .magenta, intensity: .standard) }
+    static var cyan:           Self { .standard(color: .cyan, intensity: .standard) }
+    static var white:          Self { .standard(color: .white, intensity: .standard) }
+    
+    static var lightBlack:     Self { .standard(color: .black, intensity: .bright) }
+    static var lightRed:       Self { .standard(color: .red, intensity: .bright) }
+    static var lightGreen:     Self { .standard(color: .green, intensity: .bright) }
+    static var lightYellow:    Self { .standard(color: .yellow, intensity: .bright) }
+    static var lightBlue:      Self { .standard(color: .blue, intensity: .bright) }
+    static var lightMagenta:   Self { .standard(color: .magenta, intensity: .bright) }
+    static var lightCyan:      Self { .standard(color: .cyan, intensity: .bright) }
+    static var lightWhite:     Self { .standard(color: .white, intensity: .bright) }
 }
--- Sources/Colored/Colored.swift
+++ Sources/Colored/Colored.swift
@@ -1,25 +1,42 @@
 import Foundation
 
-public extension String {
-    // MARK: - Color
-    func colored(_ color: ANSIColor) -> ANSIString {
+public protocol Colored {
+    func colored(_ color: ANSIColor) -> ANSIString
+    func effects(_ effects: ANSIEffect...) -> ANSIString
+    
+    func foreground(_ color: ColorType) -> ANSIString
+    func background(_ color: ColorType) -> ANSIString
+}
+
+// MARK: - Colored
+public extension Colored {
+    func foreground(_ color: ColorType) -> ANSIString {
+        colored(.foreground(color))
+    }
+    
+    func background(_ color: ColorType) -> ANSIString {
+        colored(.background(color))
+    }
+}
+
+// MARK: - String + Colored
+extension String: Colored {
+    public func colored(_ color: ANSIColor) -> ANSIString {
         ANSIString(content: self, codes: [color])
     }
     
-    // MARK: - Effect
-    func effects(_ effects: ANSIEffect...) -> ANSIString {
+    public func effects(_ effects: ANSIEffect...) -> ANSIString {
         ANSIString(content: self, codes: effects.reversed())
     }
 }
 
-public extension ANSIString {
-    // MARK: - Color
-    func colored(_ color: ANSIColor) -> ANSIString {
+// MARK: - ANSIString + Colored
+extension ANSIString: Colored {
+    public func colored(_ color: ANSIColor) -> ANSIString {
         ANSIString(content: self.content, codes: [color] + self.codes)
     }
     
-    // MARK: - Effect
-    func effects(_ effects: ANSIEffect...) -> ANSIString {
+    public func effects(_ effects: ANSIEffect...) -> ANSIString {
         ANSIString(content: self.content, codes: effects.reversed() + self.codes)
     }
 }
